8133629830106e3366dc202eb1ffbabb43839fb3
AdhamVibes <adhramsalleh@gmail.com>
2026-06-10 00:21:49 +0800

    Update telegram.js

--- netlify/functions/telegram.js
+++ netlify/functions/telegram.js
@@ -1,16 +1,19 @@
 exports.handler = async (event) => {
   const TG_TOKEN = process.env.TG_TOKEN;
+  const CHAT_ID = "-1003937610414";
   const body = JSON.parse(event.body);
-  
   const response = await fetch(
     `https://api.telegram.org/bot${TG_TOKEN}/sendMessage`,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body)
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: body.message,
+        parse_mode: 'HTML'
+      })
     }
   );
-  
   const data = await response.json();
   return {
     statusCode: 200,
